079f211619c4c0c79d4cfbccf316d8d688b085f8
Arthur_022 <arthurlucena328@gmail.com>
2026-09-25 19:55:10 -0300

    Refactor to use JSON for employee data storage

--- funcionario.py
+++ funcionario.py
@@ -1,17 +1,32 @@
+import json
 from pathlib import Path
 
-path_bd = Path("sincro_escala/BD") / "funcionario_bd.txt"
+path_bd = Path(__file__).parent / "BD" / "funcionario_bd.json"
 funcionarios = []
 
-def cadastrar_funcionários():
+def _carregar_fucionarios():
+    if not path_bd.exists():
+        return []
+    
+    with open(path_bd,"r", encoding="utf-8") as arquivo:
+        return json.load(arquivo)
+    
+
+def cadastrar_funcionarios():
+    funcionarios = _carregar_fucionarios()
     print("======================================")
-    print("     CAFASTRO DE FUNCIONARIOS")
+    print("     CADASTRO DE FUNCIONARIOS")
     print("======================================")
     print("")
-    funcionário = input("digite o nome do funcionario: ")
-    with open(path_bd,"a", encoding="utf-8") as arquivo:
-            arquivo.write(f"{funcionário}\n")
-    print(f"O nome cadastrado foi: {funcionário}")
+    
+    nome = input("digite o nome do funcionario: ")
+    proximo_id = max((f["id"] for f in funcionarios), default=0) + 1 
+    funcionarios.append({"id": proximo_id, "nome":nome})
+    
+    with open(path_bd,"w", encoding="utf-8") as arquivo:
+        json.dump(funcionarios ,arquivo ,ensure_ascii= False ,indent= 4)
+        
+    print(f"O nome cadastrado foi: {funcionarios}")
     print("======================================")
     print("você gostaria de adicionar um novo funcionário?")
     print("1. Sim ✅")
@@ -19,21 +34,18 @@ def cadastrar_funcionários():
     seguir_cadastro = input("Escolha uma opção: ")
     print("======================================")
     if seguir_cadastro  == "1":    
-        cadastrar_funcionários()
+        cadastrar_funcionarios()
     if seguir_cadastro == "2":
         print("cadastro concluido ✅")
         
-def listar_funcionários():
-    with open(path_bd,"r", encoding="utf-8") as arquivo:
-        for linha in arquivo:
-            nome_limpo = linha.strip()
-            if nome_limpo not in funcionarios:   
-                funcionarios.append(nome_limpo) 
+def listar_funcionarios():
+    funcionarios = _carregar_fucionarios()
+    for f in funcionarios: 
+        print(f"id: {f['id']}")   
+       
     
-    print("\n".join(funcionarios))
-            
-def excluir_funcionários():
-    listar_funcionários()
+def excluir_funcionarios():
+    listar_funcionarios()
     funcionário = input("qual funcionário você deseja deletar: ")
     with open(path_bd,"r", encoding="utf-8") as arquivo:
         nomes = arquivo.readlines()
